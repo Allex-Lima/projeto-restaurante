4a7e44d337fe96d49b80b3e7f631868134ca0519
Allex-Lima <alexlmangabeira@gmail.com>
2025-02-13 00:36:40 -0300

    feat: configuração da conexão do banco de dados

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+
+  console.log('DB_HOST: ', process.env.DB_HOST);
+  console.log('DB_PORT: ', process.env.DB_PORT);
+  console.log('DB_NAME: ', process.env.DB_NAME);
+  
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  await app.listen(process.env.DB_PORT ?? 3000);
 }
 bootstrap();
