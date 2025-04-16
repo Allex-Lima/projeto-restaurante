c0bb9fba207d625201a0771991b68f4aa9b690ad
Allex-Lima <alexlmangabeira@gmail.com>
2025-04-15 22:06:48 -0300

    feat: método - (update) atualização do produto

--- src/produto/produto.controller.ts
+++ src/produto/produto.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post, Req } from "@nestjs/common";
+import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post, Req } from "@nestjs/common";
 import { ProdutoService } from "./produto.service";
 import { CreateProdutoDto } from "./dto/create-produto.dto";
 import { Request } from "express";
+import { updateProdutoDto } from "./dto/update-produto.dto";
 
 @Controller('produtos')
 export class ProdutoController {
@@ -24,5 +25,13 @@ export class ProdutoController {
         @Req() req: Request,
         @Param('codigo', ParseIntPipe) codigo: number) {
             return this.produtoService.findOneProduto(req, codigo);
+    }
+
+    @Patch(':codigo')
+    async update(
+        @Req() req: Request,
+        @Param('codigo', ParseIntPipe) codigo: number,
+        @Body() body: updateProdutoDto) {
+            return await this.produtoService.updateProduto(req, codigo, body);
     }
 }
